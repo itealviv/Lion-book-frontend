95877cf45e4a61b567595c07586f81e2c5f57ce5
Maievskyi Vadym <30689925+VadymMai@users.noreply.github.com>
2019-10-08 00:49:25 +0300

    page login

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 import { AuthorsComponent } from './authors/authors.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AuthorComponent } from './author/author.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AuthorsComponent, HomeComponent, NotFoundComponent, AuthorComponent],
+  declarations: [AuthorsComponent, HomeComponent, NotFoundComponent, AuthorComponent, LoginComponent],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSnackBarModule
   ]
 })
 export class PagesModule { }
